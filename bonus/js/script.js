4c72cbf6988f2f7cbc8759d44e2249d912164d9c
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-07 19:24:49 +0100

    add preview

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -15,19 +15,25 @@ $(document).ready(function () {
 
         click: function(){
             var posizioneimg = $(".slider-wrapper .images  img.active");
-            var posizionenav = $(".nav i.active");
-
+            var posizionenav = $(".nav img.active");
+            ;
+            
             posizioneimg.removeClass("active");
             posizionenav.removeClass("active");
+            posizionenav.removeClass("border");
+            
+            
             if (posizioneimg.hasClass("last")) {
                 $(".slider-wrapper .images  img.first").addClass("active");
-                $(".nav i.first").addClass("active");
+                $(".nav img.first").addClass("active border");
             } else {
                 //altrimenti passa semplicemente l'active alla prossima
                 posizioneimg.next("img").addClass("active");
-                posizionenav.next("i").addClass("active");
+                posizionenav.next("img").addClass("active border");
+                
             }
-
+            
+            
         },
         mouseenter: function () {
             $(this).css("color", "orange")
@@ -35,6 +41,7 @@ $(document).ready(function () {
         mouseleave: function () {
             $(this).css("color", "#565a5c")
         }
+        
     
     });
     
@@ -44,19 +51,20 @@ $(document).ready(function () {
 
         click :function() {
         var posizioneimg = $(".slider-wrapper .images  img.active");
-        var posizionenav = $(".nav i.active");
+        var posizionenav = $(".nav img.active");
 
         posizioneimg.removeClass("active");
         posizionenav.removeClass("active");
+        posizionenav.removeClass("border");
 
         // se siamo alla prima img dello slider, ritorna ad essere attiva l'ultima
         if (posizioneimg.hasClass("first")) {
             $(".slider-wrapper .images  img.last").addClass("active");
-            $(".nav i.last").addClass("active");
+            $(".nav img.last").addClass("active border");
         } else {
             //altrimenti passa semplicemente l'active alla prossima
             posizioneimg.prev("img").addClass("active");
-            posizionenav.prev("i").addClass("active");
+            posizionenav.prev("img").addClass("active border");
         }
     },
     mouseenter : function(){
@@ -69,4 +77,8 @@ $(document).ready(function () {
 
 
     });
+
+    
+
+    
 })
