84b50ee60f355182d29e2b42d8e7ceb9aa2efe36
Mr2803 <rispoli.mar2803@gmail.com>
2019-11-07 16:50:30 +0100

    fix layout , now it's responsive

---
+++ bonus/js/script.js
@@ -0,0 +1,71 @@
+/*
+DESCRIZIONE:
+    RI-Creare lo slideshow / carousel base come fatto in live coding a lezione stamattina.
+BONUS:
+    navigare tramite tastiera;
+    generare contenuti / img(tipo da array nome imgs) + pallini;
+    aggiungere transizioni;
+    thumbnails / miniature(navigabili o meno);
+    varie ed eventuali che vi possono venire in mente.*/
+
+$(document).ready(function () {
+
+
+    $(".next").click(prossimaslide);
+    
+
+    $(".prev").click(precedenteslide);
+
+
+
+
+    // funzioni
+
+    function prossimaslide() {
+      
+
+        var posizioneimg = $(".slider-wrapper .images  img.active");
+        var posizionenav = $(".nav i.active");
+
+        posizioneimg.removeClass("active");
+        posizionenav.removeClass("active");
+
+
+        // se siamo all'utima img dello slider, ritorna ad essere attiva la prima
+        if (posizioneimg.hasClass("last")) {
+            $(".slider-wrapper .images  img.first").addClass("active");
+            $(".nav i.first").addClass("active");
+        } else {
+            //altrimenti passa semplicemente l'active alla prossima
+            posizioneimg.next("img").addClass("active");
+            posizionenav.next("i").addClass("active");
+        }
+
+
+    }
+
+
+    function precedenteslide() {
+
+        var posizioneimg = $(".slider-wrapper .images  img.active");
+        var posizionenav = $(".nav i.active");
+
+        posizioneimg.removeClass("active");
+        posizionenav.removeClass("active");
+
+        // se siamo alla prima img dello slider, ritorna ad essere attiva l'ultima
+        if (posizioneimg.hasClass("first")) {
+            $(".slider-wrapper .images  img.last").addClass("active");
+            $(".nav i.last").addClass("active");
+        } else {
+            //altrimenti passa semplicemente l'active alla prossima
+            posizioneimg.prev("img").addClass("active");
+            posizionenav.prev("i").addClass("active");
+        }
+
+
+    }
+
+
+});
+
